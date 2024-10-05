5dc579c5791b0bb8fd253b74f925d87a0c7f0249
Rocío Baigorria <97059658+rb555@users.noreply.github.com>
2024-10-05 11:10:07 -0300

    uploading packages and app.java

---
+++ PROJECT/model/Expense.java
@@ -0,0 +1,43 @@
+package com.example.expensetracker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    private BigDecimal amount;
+}
